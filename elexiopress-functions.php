efaefbb82dd6d5bd583ed5c69a6223da21d917b3
brettgoodrich <brettgoodrich@horizonindy.org>
2016-09-30 16:31:51 -0400

    first attempt at API connection

--- elexiopress-functions.php
+++ elexiopress-functions.php
@@ -12,40 +12,13 @@ function print_r2($val){
         echo  '</pre>';
 }
 
-function ep_getPerson($person) {
-	?>
-	var oPostData = {
-	    ActivationKey: <?php echo $elexiopress_settings[elexiopress_keys_activationkey]; ?>,
-	    APIPass: <?php echo $elexiopress_settings[elexiopress_keys_apipass]; ?>,
-	    SearchString: <?php echo $person; ?>
-	};
-
-	$jQuery.ajax({
-	    url: "https://www.elexioamp.com/Services/Database/API.asmx/FindPersonByName",
-	    type: "POST",
-	    contentType: "application/json; charset=utf-8",
-	    data: JSON.stringify(oPostData),
-	    dataType: "json"
-	}).done(function (msg) {
-	    oModel = $(msg.d);
-
-	    oModel.each(function (index, person) {
-	        var sPerson = "Contact ID: " + person.ContactID + "\n";
-	        sPerson += "Display Name: " + person.DisplayName + "\n";
-	        sPerson += "Household ID: " + person.HouseholdID + "\n";
-	        sPerson += "Household Name: " + person.HouseholdName + "\n";
-	        alert(sPerson);
-	        /*
-	        This will popup an alert for each person found by your query that
-	        looks something like this...
-	        --------------------------------------
-	        Contact ID: 12345
-	        Display Name: John Doe
-	        Household ID: 67890
-	        Household Name: Doe, John & Jane
-	        */
-	    });
-	});
-	<?php
+function elexiopress_getPerson($input) {
+	$api_url = 'https://www.elexioamp.com/Services/Database/API.asmx/FindPersonByName';
+	$headers = 'ActivationKey='.$elexiopress_settings['elexiopress_keys_activationkey']
+	.'&APIPass='.$elexiopress_settings['elexiopress_keys_apipass']
+	.'SearchString'.$input;
+	$request = new WP_Http;
+	$result = $request->request( $api_url , array( 'method' => 'POST', 'headers' => $headers ) );
+	print_r2($result);
 }
 ?>
